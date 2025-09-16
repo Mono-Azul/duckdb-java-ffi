11eb2d0f83e2d87aeba6254df32d2d7451f259f0
Jens Hofer <jens.hofer@mono-azul.de>
2025-09-16 21:53:43 +0200

    Add Boolean, Byte, Float, Double ObjectColumns

---
+++ src/monoazul/duckdbffi/driver/ByteObjectColumn.java
@@ -0,0 +1,35 @@
+package monoazul.duckdbffi.driver;
+
+import java.lang.foreign.MemorySegment;
+import java.lang.foreign.ValueLayout;
+import java.util.Arrays;
+
+import static monoazul.duckdbffi.jextractffi.duckdb_h.duckdb_vector_get_data;
+
+public class ByteObjectColumn extends ObjectColumn<Byte>
+{
+    public ByteObjectColumn (String ColumnName, DuckDbDatatype ColumnDatatype)
+    {
+        super(ColumnName, ColumnDatatype);
+        Clazz = Byte.class;
+    }
+
+    @Override
+    protected void addVectorChunk(MemorySegment ResultVector, int dbChunkSize)
+    {
+        // Convert Vector into byte[] array first and then convert to Byte[]
+        MemorySegment ResultVectorData = duckdb_vector_get_data(ResultVector);
+        byte[] PrimitiveResultArray = new byte[dbChunkSize];
+        ResultVectorData.reinterpret((long) dbChunkSize * 4);
+        MemorySegment.copy(ResultVectorData, ValueLayout.JAVA_BYTE, 0, PrimitiveResultArray, 0, dbChunkSize);
+
+        Byte[] ResultArray = new Byte[dbChunkSize];
+        for (int pos = 0; pos < dbChunkSize; pos++)
+        {
+            ResultArray[pos] = PrimitiveResultArray[pos];
+        }
+
+        setValidityForChunk(ResultVector, dbChunkSize, ResultArray);
+        this.VectorArrays.add(ResultArray);
+    }
+}
---
+++ src/monoazul/duckdbffi/driver/FloatObjectColumn.java
@@ -0,0 +1,35 @@
+package monoazul.duckdbffi.driver;
+
+import java.lang.foreign.MemorySegment;
+import java.lang.foreign.ValueLayout;
+import java.util.Arrays;
+
+import static monoazul.duckdbffi.jextractffi.duckdb_h.duckdb_vector_get_data;
+
+public class FloatObjectColumn extends ObjectColumn<Float>
+{
+    public FloatObjectColumn (String ColumnName, DuckDbDatatype ColumnDatatype)
+    {
+        super(ColumnName, ColumnDatatype);
+        Clazz = Float.class;
+    }
+
+    @Override
+    protected void addVectorChunk(MemorySegment ResultVector, int dbChunkSize)
+    {
+        // Convert Vector into float[] array first and then convert to Float[]
+        MemorySegment ResultVectorData = duckdb_vector_get_data(ResultVector);
+        float[] PrimitiveResultArray = new float[dbChunkSize];
+        ResultVectorData.reinterpret((long) dbChunkSize * 4);
+        MemorySegment.copy(ResultVectorData, ValueLayout.JAVA_FLOAT, 0, PrimitiveResultArray, 0, dbChunkSize);
+
+        Float[] ResultArray = new Float[dbChunkSize];
+        for (int pos = 0; pos < dbChunkSize; pos++)
+        {
+            ResultArray[pos] = PrimitiveResultArray[pos];
+        }
+
+        setValidityForChunk(ResultVector, dbChunkSize, ResultArray);
+        this.VectorArrays.add(ResultArray);
+    }
+}
