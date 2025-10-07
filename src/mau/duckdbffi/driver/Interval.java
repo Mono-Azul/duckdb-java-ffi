03c5657274b5b95d472716e800bb24fdc81f83ff
Jens Hofer <jens.hofer@mono-azul.de>
2025-10-07 21:57:21 +0200

    Add comparator for Interval record

--- src/mau/duckdbffi/driver/Interval.java
+++ src/mau/duckdbffi/driver/Interval.java
@@ -2,9 +2,19 @@ package mau.duckdbffi.driver;
 
 import java.time.Duration;
 import java.time.Period;
+import java.util.Comparator;
 
 // An Interval has a Period for intervals >= day and a Duration for interval parts < day.
 // The objects must not be null, but initialized with 0
 public record Interval(Period Period, Duration Duration)
 {
+    public static final Comparator<Interval> COMPARATOR =
+            // Compare Period's Years
+            Comparator.comparing((Interval i) -> i.Period().getYears())
+                    // Then compare Period's Months
+                    .thenComparing((Interval i) -> i.Period().getMonths())
+                    // Then compare Period's Days
+                    .thenComparing((Interval i) -> i.Period().getDays())
+                    // Finally, compare Duration
+                    .thenComparing(Interval::Duration);
 }
